ff6276ff78c1ab800ce6bfaafde5a969a2b34c62
Ruth <ruth.goh@kcl.ac.uk>
2024-11-07 22:48:28 +0000

    sample poly cbd

--- src/convert_compress.rs
+++ src/convert_compress.rs
@@ -8,7 +8,7 @@ fn bits_to_bytes(bit_vec:&Vec<u8>) -> Vec<u8> {
     byte_vec
 }
 
-fn bytes_to_bits(byte_vec:&Vec<u8>) -> Vec<u8> {
+pub fn bytes_to_bits(byte_vec:&Vec<u8>) -> Vec<u8> {
     let mut bit_vec: Vec<u8> = Vec::with_capacity(byte_vec.len()*8);
     byte_vec.iter().for_each(
         |e| {(0..8).for_each(|i| bit_vec.push(e>>i & 1))}
@@ -34,8 +34,8 @@ fn decompress(mod_exp:&u8, vec:&mut Vec<u16>) {
 
 // m=2^d if d<12, m=q if d=12
 // n=256
-fn byte_encode(mod_exp:&u8, int_vec:&Vec<u16>) -> Vec<u8> {
-    let mod_exp: usize = *mod_exp as usize;
+fn byte_encode(mod_exp:u8, int_vec:&Vec<u16>) -> Vec<u8> {
+    let mod_exp: usize = mod_exp as usize;
     let mut bit_vec: Vec<u8> = Vec::with_capacity(32*mod_exp);
     int_vec.iter().for_each(
         |e| {(0..mod_exp).for_each(|j| bit_vec.push((e>>j) as u8 & 1))}
@@ -44,13 +44,13 @@ fn byte_encode(mod_exp:&u8, int_vec:&Vec<u16>) -> Vec<u8> {
 }
 
 // n=256, q=3329
-fn byte_decode(mod_exp:&u8, byte_vec:&Vec<u8>) -> Vec<u16> {
+fn byte_decode(mod_exp:u8, byte_vec:&Vec<u8>) -> Vec<u16> {
     let modulus: u16;
-    if *mod_exp == 12 {modulus = 3329;}
+    if mod_exp == 12 {modulus = 3329;}
     else {modulus = 1<<mod_exp;}
     
     let mut int_vec: Vec<u16> = Vec::with_capacity(256);
-    int_vec.extend(bytes_to_bits(byte_vec).chunks_exact(*mod_exp as usize).map(
+    int_vec.extend(bytes_to_bits(byte_vec).chunks_exact(mod_exp as usize).map(
         |chunk| -> u16 {chunk.iter().enumerate().map(
             |(i,e)| (*e as u16)<<i
         ).sum()}
--- src/sample.rs
+++ src/sample.rs
@@ -1,7 +1,20 @@
-fn sample_NTT(seed_vec:Vec<u8>) -> Vec<u16> {
-    return Vec::new();
+use crate::convert_compress::bytes_to_bits;
+
+fn sample_NTT(seed_vec:&Vec<u8>) -> Vec<u16> {
+    Vec::new()
 }
 
-fn sample_poly_CBD(param_n:u8, seed_vec:Vec<u8>) -> Vec<u16> {
-    return Vec::new();
+// q=3329
+fn sample_poly_CBD(eta:u8, seed_vec:&Vec<u8>) -> Vec<u16> {
+    let eta = eta as usize;
+    let mut sample_vec: Vec<u16> = Vec::with_capacity(256);
+    sample_vec.extend(
+        bytes_to_bits(seed_vec).into_iter().map(|e| e as u16).collect::<Vec<u16>>()
+        .chunks_exact(eta*2).map(
+            |chunk| -> u16{
+                let mut it = chunk.iter();
+                (it.by_ref().take(eta).sum::<u16>() - it.sum::<u16>())%3329
+            }
+    ));
+    sample_vec
 }
