a6c54795bc35473a62e8f16a3ac2ee9b98e23947
Ruth <ruth.goh@kcl.ac.uk>
2025-04-02 05:41:40 +0100

    Add docs

---
+++ doc/trait.impl/zeroize/trait.Zeroize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["secrecy",[["impl&lt;S: <a class=\"trait\" href=\"zeroize/trait.Zeroize.html\" title=\"trait zeroize::Zeroize\">Zeroize</a> + ?<a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"zeroize/trait.Zeroize.html\" title=\"trait zeroize::Zeroize\">Zeroize</a> for <a class=\"struct\" href=\"secrecy/struct.SecretBox.html\" title=\"struct secrecy::SecretBox\">SecretBox</a>&lt;S&gt;"]]],["zeroize",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[493,15]}
